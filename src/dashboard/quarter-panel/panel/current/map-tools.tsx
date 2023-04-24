43a8e14cf46efde4453bba12981ecc9e02eb8b13
Dylan Green <dylangreencoding@gmail.com>
2023-04-23 22:27:02 -0400

    in deployment

--- src/dashboard/quarter-panel/panel/current/map-tools.tsx
+++ src/dashboard/quarter-panel/panel/current/map-tools.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { userRoute } from "../../../../expressAPI/user-route";
 
-
 interface Props {
   setCurrent: Function;
   setTab: Function;
@@ -16,156 +15,186 @@ interface Props {
   getUserData: Function;
 }
 
-export default function MapTools (props: Props) {
-
+export default function MapTools(props: Props) {
   const [mapName, setmapName] = useState<string>(props.savedMap.name);
 
   let currentMap = props.savedMap;
-  
+
   const handleSaveMap = async (e: any) => {
     e.preventDefault();
     currentMap.name = mapName;
     currentMap.currentMap = {};
 
-    const route = 'save-map';
+    const route = "save-map";
     await userRoute(route, props.accessToken, currentMap);
     await props.getUserData();
-  }
+  };
 
   const handleHostGame = async (e: any) => {
     e.preventDefault();
     const gameMap = props.savedMap;
-    gameMap.id = '';
-    gameMap.messages = []
-    gameMap.players = {}
-    gameMap.players[props.user.user.id] = 'host';
+    gameMap.id = "";
+    gameMap.messages = [];
+    gameMap.players = {};
+    gameMap.players[props.user.user.id] = "host";
     gameMap.currentMap = {};
     gameMap.selected = {};
-    gameMap.tool = 'none';
+    gameMap.tool = "none";
 
-    const route = 'save-game';
+    const route = "save-game";
     const response = await userRoute(route, props.accessToken, gameMap);
     await props.getUserData();
     const currentGame = response.game;
     props.setSavedGame(currentGame);
 
-
-    props.setCurrent('game');
-    props.setTab('current');
-  }
+    props.setCurrent("game");
+    props.setTab("current");
+  };
 
   const handlePickTool = (e: any) => {
     const currentMap = props.savedMap;
     currentMap.tool = e.target.value;
-    props.setSavedMap({...props.savedMap, currentMap});
-  }
+    props.setSavedMap({ ...props.savedMap, currentMap });
+  };
 
   const handleUndoLine = (e: any) => {
     const currentMap = props.savedMap;
     currentMap.lines.pop();
-    props.setSavedMap({...props.savedMap, currentMap});
-  }
+    props.setSavedMap({ ...props.savedMap, currentMap });
+  };
 
   const getSelected = () => {
     const locationToString = (location: any) => {
       const x = location.x.toString();
       const y = location.y.toString();
-      const xy = x.concat(' ', y);
-      return xy
-    }
-    
+      const xy = x.concat(" ", y);
+      return xy;
+    };
+
     let selected;
-    if (props.savedMap.selected.x !== undefined && props.savedMap.selected.y !== undefined) {
-      selected = props.savedMap.selectFrom[locationToString(props.savedMap.selected)];
+    if (
+      props.savedMap.selected.x !== undefined &&
+      props.savedMap.selected.y !== undefined
+    ) {
+      selected =
+        props.savedMap.selectFrom[locationToString(props.savedMap.selected)];
       if (selected != undefined) {
-        return selected.type
+        return selected.type;
       } else {
-        return 'empty square'
+        return "empty square";
       }
     } else {
-      return 'none selected'
+      return "none selected";
     }
-  }
-
+  };
 
   return (
-    <div className='map-tools'>
-      
-      <div className='mb24 tool-box'>
+    <div className="map-tools">
+      <div className="mb24 tool-box">
         <h3>{props.savedMap.name}</h3>
-        { props.user.games.length < 1 ?
-        <button type='button' className="btn" onClick={handleHostGame}>host game</button> :
-        <span>1/1, games full</span> }
+        {props.user.games.length! < 1 ? (
+          <button type="button" className="btn" onClick={handleHostGame}>
+            host game
+          </button>
+        ) : (
+          <span>1/1, games full</span>
+        )}
       </div>
 
       <div>
         <div>
           <div className="mb24">
-            <div>{props.savedMap.selected.x}, {props.savedMap.selected.y}</div>
+            <div>
+              {props.savedMap.selected.x}, {props.savedMap.selected.y}
+            </div>
             <div>{getSelected()}</div>
           </div>
 
           <div className="tool-box mb24">
             <div>
-              <button 
-                type='button'
-                className={`btn ${props.savedMap.tool === 'none' ? 'active' : ''}`}
-                value='none'
+              <button
+                type="button"
+                className={`btn ${
+                  props.savedMap.tool === "none" ? "active" : ""
+                }`}
+                value="none"
                 onClick={handlePickTool}
-              >select</button>
+              >
+                select
+              </button>
             </div>
             <div>
-              <button 
-                type='button'
-                className={`btn ${props.savedMap.tool === 'add line' ? 'active' : ''}`}
-                value='add line'
+              <button
+                type="button"
+                className={`btn ${
+                  props.savedMap.tool === "add line" ? "active" : ""
+                }`}
+                value="add line"
                 onClick={handlePickTool}
-              >add line</button>
+              >
+                add line
+              </button>
               <span>---</span>
-              <button 
-                type='button'
-                className='btn'
-                value='undo-line'
+              <button
+                type="button"
+                className="btn"
+                value="undo-line"
                 onClick={handleUndoLine}
-              >undo line</button>
+              >
+                undo line
+              </button>
             </div>
             <div>
-              <button 
-                type='button'
-                className={`btn ${props.savedMap.tool === 'add location' ? 'active' : ''}`}
-                value='add location'
+              <button
+                type="button"
+                className={`btn ${
+                  props.savedMap.tool === "add location" ? "active" : ""
+                }`}
+                value="add location"
                 onClick={handlePickTool}
-              >add location</button>
+              >
+                add location
+              </button>
             </div>
             <div>
-              <button 
-                type='button'
-                className={`btn ${props.savedMap.tool === 'move' ? 'active' : ''}`}
-                value='move'
+              <button
+                type="button"
+                className={`btn ${
+                  props.savedMap.tool === "move" ? "active" : ""
+                }`}
+                value="move"
                 onClick={handlePickTool}
-              >move</button>
+              >
+                move
+              </button>
             </div>
             <div>
-              <button 
-                type='button'
-                className={`btn ${props.savedMap.tool === 'shoot' ? 'active' : ''}`}
-                value='shoot'
+              <button
+                type="button"
+                className={`btn ${
+                  props.savedMap.tool === "shoot" ? "active" : ""
+                }`}
+                value="shoot"
                 onClick={handlePickTool}
-              >shoot</button>
+              >
+                shoot
+              </button>
             </div>
           </div>
         </div>
-        <form className="text-form tool-box" onSubmit={handleSaveMap} >
-          <input 
-            className='text-input' 
-            type='text' 
-            placeholder='*name your map*'
+        <form className="text-form tool-box" onSubmit={handleSaveMap}>
+          <input
+            className="text-input"
+            type="text"
+            placeholder="*name your map*"
             value={mapName}
-            onChange={ (e) => setmapName(e.target.value) }
+            onChange={(e) => setmapName(e.target.value)}
           />
-          <button type='submit' className="btn">save</button>
+          <button type="submit" className="btn">
+            save
+          </button>
         </form>
       </div>
     </div>
-  )
+  );
 }
